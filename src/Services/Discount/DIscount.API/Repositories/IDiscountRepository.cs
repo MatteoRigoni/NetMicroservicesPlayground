2fbb50ce3097db5812bbac75f131bac55f0381d9
LAPTOP-MTO\Matteo <matteo.rigoni2@gmail.com>
2022-02-24 22:48:07 +0100

    DIscount.API

---
+++ src/Services/Discount/DIscount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using DIscount.API.Entities;
+
+namespace DIscount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
