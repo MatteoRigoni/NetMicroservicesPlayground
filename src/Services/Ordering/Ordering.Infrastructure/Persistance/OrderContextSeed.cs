390a84390e760a1f6ce259c5e490dfd75394455f
LAPTOP-MTO\Matteo <matteo.rigoni2@gmail.com>
2022-02-26 18:10:26 +0100

    Ordering.INfrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Seed database associated with context {nameof(OrderContext)}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "fb", FirstName = "frodo", LastName = "baggins", EmailAddress = "test@gmail.com", AddressLine = "3", Country = "Contea", TotalPrice = 350 }
+            };
+        }
+    }
+}
